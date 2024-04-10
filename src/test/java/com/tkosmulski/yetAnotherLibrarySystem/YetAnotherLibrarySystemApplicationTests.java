ee85aaf2575e69615e62f4f30e52eef0b6e22cdf
WiadroMasla <tomasz.kosmulski@student.uj.edu.pl>
2024-04-10 19:42:01 +0200

    Added initializr skeleton

---
+++ src/test/java/com/tkosmulski/yetAnotherLibrarySystem/YetAnotherLibrarySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tkosmulski.yetAnotherLibrarySystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YetAnotherLibrarySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
